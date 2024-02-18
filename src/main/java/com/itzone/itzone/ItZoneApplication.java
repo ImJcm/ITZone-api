65160a8dcab6f3edf626ea6024ffc4d8e8763015
ImJcm <wjsckdals0909@naver.com>
2024-02-18 15:47:04 +0900

    init Setting

---
+++ src/main/java/com/itzone/itzone/ItZoneApplication.java
@@ -0,0 +1,13 @@
+package com.itzone.itzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItZoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItZoneApplication.class, args);
+    }
+
+}
