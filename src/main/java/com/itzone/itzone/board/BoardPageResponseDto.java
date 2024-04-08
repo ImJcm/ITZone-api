77939fbd546efdec46b41acdf2d48f3a7c3d3404
ImJcm <wjsckdals0909@naver.com>
2024-04-08 18:32:30 +0900

    feature: 27-BoardPageResponseDto @AllArgsConstructor, @NoArgsConstructor 추가

--- src/main/java/com/itzone/itzone/board/BoardPageResponseDto.java
+++ src/main/java/com/itzone/itzone/board/BoardPageResponseDto.java
@@ -1,13 +1,14 @@
 package com.itzone.itzone.board;
 
 import com.itzone.itzone.common.PageInfo;
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
 import java.util.List;
 
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BoardPageResponseDto {
     List<BoardResponseDto> content;
     PageInfo pageInfo;
